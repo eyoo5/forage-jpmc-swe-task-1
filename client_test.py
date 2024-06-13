e1c94531fb3716bcecd46af10acc47ef463a855b
Esther Yoo <liletchie@gmail.com>
2024-06-13 18:26:06 -0400

    added tests

--- client_test.py
+++ client_test.py
@@ -8,6 +8,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    results = [getDataPoint(quote) for quote in quotes]
+
+    expected_results = [("ABC", 120.48, 121.2, (120.48 + 121.2)/2 ),( "DEF", 117.87, 121.68, (117.87 + 121.68)/2)]
+    self.assertEqual(results, expected_results)
 
   def test_getDataPoint_calculatePriceBidGreaterThanAsk(self):
     quotes = [
@@ -15,7 +19,10 @@ class ClientTest(unittest.TestCase):
       {'top_ask': {'price': 121.68, 'size': 4}, 'timestamp': '2019-02-11 22:06:30.572453', 'top_bid': {'price': 117.87, 'size': 81}, 'id': '0.109974697771', 'stock': 'DEF'}
     ]
     """ ------------ Add the assertion below ------------ """
+    results = [getDataPoint(quote) for quote in quotes]
 
+    expected_results = [("ABC", 120.48, 119.2, (120.48 + 119.2) / 2), ("DEF", 117.87, 121.68, (117.87 + 121.68) / 2)]
+    self.assertEqual(results, expected_results)
 
   """ ------------ Add more unit tests ------------ """
 
